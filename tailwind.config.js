b1406aa5493eafc13a0d5b4243120c24248c6fb4
DESKTOP-PS9EO7I\Usuario <sergiotomasr@gmail.com>
2025-06-27 08:05:34 +0200

    añadiendo cosas nuevas

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+module.exports = {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // ajusta rutas si es necesario
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  

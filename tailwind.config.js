9ae0f0e248bfd04981d83ee99d4c1440c4b88850
DESKTOP-PS9EO7I\Usuario <sergiotomasr@gmail.com>
2025-07-07 12:20:29 +0200

    responsive design complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 // tailwind.config.js
 module.exports = {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // ajusta rutas si es necesario
+    darkMode: 'class', // habilita el modo oscuro basado en clases
     theme: {
       extend: {},
     },
